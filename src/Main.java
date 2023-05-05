1aad8bfddd85bc825d1ab27b2ceef95d5072b6c7
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-05-05 23:52:30 +0530

    main-form-create

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome CBL!");
+
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
+
     }
 }
---
+++ src/MainForm.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+    private JPanel mainPanel;
+
+    public MainForm() {
+        add(mainPanel);
+        setTitle("CBL - Main Form Application");
+        setSize(1280, 1080);
+    }
+
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
+    }
+}
